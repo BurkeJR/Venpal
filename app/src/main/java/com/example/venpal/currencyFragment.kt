448c41d2a64468b5574e168340c9e88c2e4fa082
Luke Greenway <greenwaylj18@gcc.edu>
2021-11-16 14:31:58 -0500

    Fixed my code from being in stocks to being in currency

--- app/src/main/java/com/example/venpal/currencyFragment.kt
+++ app/src/main/java/com/example/venpal/currencyFragment.kt
@@ -5,55 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.venpal.databinding.FragmentCurrencyBinding
+import com.example.venpal.databinding.FragmentStocksBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [currencyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class currencyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentCurrencyBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_currency, container, false)
+        binding = FragmentCurrencyBinding.inflate(inflater)
+        binding.convertButton.setOnClickListener { convert() }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment currencyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            currencyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    private fun convert(){
+        val usd = binding.USDnumTV.text.toString()
+        try {
+            var num = usd.toDouble()
+            if(num < 0){
+                throw Exception("Negative")
             }
+            binding.stocksErrorTV.text = ""
+            num *= .88
+            binding.foreignMoneyTV.text = String.format("%.2f", num)
+
+
+        } catch (nfe: Exception) {
+            binding.stocksErrorTV.text = "PLEASE ENTER ONLY NUMBER VALUES"
+        }
+
     }
 }
--- app/src/main/java/com/example/venpal/stocksFragment.kt
+++ app/src/main/java/com/example/venpal/stocksFragment.kt
@@ -10,33 +10,4 @@ import com.example.venpal.databinding.FragmentStocksBinding
 
 class stocksFragment : Fragment() {
 
-    private lateinit var binding: FragmentStocksBinding
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentStocksBinding.inflate(inflater)
-        binding.convertButton.setOnClickListener { convert() }
-
-        return binding.root
-    }
-
-    private fun convert(){
-        val usd = binding.USDnumTV.text.toString()
-        try {
-            var num = usd.toDouble()
-            if(num < 0){
-                throw Exception("Negative")
-            }
-            binding.stocksErrorTV.text = ""
-            num *= .88
-            binding.foreignMoneyTV.text = String.format("%.2f", num)
-
-
-        } catch (nfe: Exception) {
-            binding.stocksErrorTV.text = "PLEASE ENTER ONLY NUMBER VALUES"
-        }
-
-    }
 }
